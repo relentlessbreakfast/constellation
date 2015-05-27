8f7effe2e1265db036db2f90212a9da74baef3bc
Justin Webb <jw@justinwebb.io>
2015-05-27 11:04:31 -0700

    Clean up demo code

--- client/app/app.js
+++ client/app/app.js
@@ -2,7 +2,11 @@
 * @Author: justinwebb
 * @Date:   2015-05-22 19:51:08
 * @Last Modified by:   justinwebb
-* @Last Modified time: 2015-05-22 19:51:08
+* @Last Modified time: 2015-05-26 23:49:32
 */
 
 'use strict';
+
+
+function test(doo) {
+};
