6b34a9fd543b5d411d3465dfe2e64dff2abc37d0
Justin Webb <jw@justinwebb.io>
2015-05-27 17:43:51 -0700

    (feat) Add clean and build tasks to build process

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 * @Author: justinwebb
 * @Date:   2015-05-26 15:18:17
 * @Last Modified by:   justinwebb
-* @Last Modified time: 2015-05-27 11:01:32
+* @Last Modified time: 2015-05-27 17:39:19
 */
 
 'use strict';
@@ -14,8 +14,10 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
-var open = require('gulp-open');
+var clean = require('gulp-clean');
+var copy = require('gulp-copy');
 var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
 var config = require('./build-config');
 var browserSyncReload = browserSync.reload;
 
@@ -29,7 +31,6 @@ var serveBuildFiles = function () {
     }
   });
 };
-
 
 var compileSassFiles = function () {
   gulp.src(config.appFiles.scss)
@@ -45,11 +46,29 @@ var compileSassFiles = function () {
     .pipe(browserSyncReload({stream: true}));
 };
 
+var cleanPreviousBuild = function () {
+  return gulp.src(config.build)
+    .pipe(clean());
+};
+
+var copySrcFilesToBuild = function () {
+  var options = {prefix: 0};
+  var buildFiles = [];
+
+  buildFiles = config.vendorFiles.js.concat(config.appFiles.js, config.data);
+  console.log('copySrcFilesToBuild: ', buildFiles);
+  return gulp.src(buildFiles)
+    .pipe(copy(config.build, options));
+};
+
 // ---------------------------------------------------------
 // Register tasks
 // ---------------------------------------------------------
-gulp.task('sass', compileSassFiles);
+gulp.task('clean', cleanPreviousBuild);
 
+gulp.task('copy', copySrcFilesToBuild);
+
+gulp.task('sass', compileSassFiles);
 
 gulp.task('serve', ['sass'], function () {
 
