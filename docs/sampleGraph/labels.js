701c104216e2a368a9e1196692822465c1c3b985
Katrina Uychaco <kuychaco@gmail.com>
2015-05-31 13:32:24 -0700

    Change repo_id for dummy data

--- docs/sampleGraph/labels.js
+++ docs/sampleGraph/labels.js
@@ -2,7 +2,7 @@
 * @Author: kuychaco
 * @Date:   2015-05-29 22:01:07
 * @Last Modified by:   kuychaco
-* @Last Modified time: 2015-05-29 23:08:59
+* @Last Modified time: 2015-05-30 15:50:13
 */
 
 'use strict';
@@ -13,7 +13,7 @@ var label = {
   "url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/labels/bug",
   "name": "bug",
   "color": "fc2929",
-  "repo_id": "relentlessbreakfast/sampleGraph"
+  "repo_id": 1
 };
 
 // DATABASE SCHEMA
@@ -23,49 +23,49 @@ var labels = [
   "url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/labels/bug",
   "name": "bug",
   "color": "fc2929"
-  "repo_id": "relentlessbreakfast/sampleGraph"
+  "repo_id": 1
   },
   {
   "id": 2,  // PRIMARY KEY
   "url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/labels/duplicate",
   "name": "duplicate",
   "color": "cccccc"
-  "repo_id": "relentlessbreakfast/sampleGraph"
+  "repo_id": 1
   },
   {
   "id": 3,  // PRIMARY KEY
   "url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/labels/enhancement",
   "name": "enhancement",
   "color": "84b6eb"
-  "repo_id": "relentlessbreakfast/sampleGraph"
+  "repo_id": 1
   },
   {
   "id": 4,  // PRIMARY KEY
   "url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/labels/help%20wanted",
   "name": "help wanted",
   "color": "159818"
-  "repo_id": "relentlessbreakfast/sampleGraph"
+  "repo_id": 1
   },
   {
   "id": 5,  // PRIMARY KEY
   "url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/labels/invalid",
   "name": "invalid",
   "color": "e6e6e6"
-  "repo_id": "relentlessbreakfast/sampleGraph"
+  "repo_id": 1
   },
   {
   "id": 6,  // PRIMARY KEY
   "url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/labels/question",
   "name": "question",
   "color": "cc317c"
-  "repo_id": "relentlessbreakfast/sampleGraph"
+  "repo_id": 1
   },
   {
   "id": 7,  // PRIMARY KEY
   "url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/labels/wontfix",
   "name": "wontfix",
   "color": "ffffff",
-  "repo_id": "relentlessbreakfast/sampleGraph"
+  "repo_id": 1
   }
 ];
 
