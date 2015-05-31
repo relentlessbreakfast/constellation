5b54e144e08d8514f02b371a6c5ea214dd142a32
Katrina Uychaco <kuychaco@gmail.com>
2015-05-31 13:33:39 -0700

    (feat) Create schema for database

--- server/database/postgres-schema.sql
+++ server/database/postgres-schema.sql
@@ -1,198 +1,170 @@
 /* 
 * @Author: kuychaco
-* @Date:   2015-05-29 10:54:46
+* @Date:   2015-05-30 15:14:34
 * @Last Modified by:   kuychaco
-* @Last Modified time: 2015-05-29 22:38:07
+* @Last Modified time: 2015-05-30 17:12:03
 */
 
-/*
 
- */
+CREATE EXTENSION hstore;
 
 -- ---
 -- Table 'users'
---   Synced with GitHub API
+--   Foreign keys: repo_list elements (not enforced)
 -- ---
 
 DROP TABLE IF EXISTS users;
 
 CREATE TABLE users (
-  login VARCHAR, -- "kuychaco",
-  id INTEGER, -- 7910250,
-  avatar_url VARCHAR DEFAULT NULL, -- "https://avatars.githubusercontent.com/u/7910250?v=3",
-  url VARCHAR DEFAULT NULL, -- "https://api.github.com/users/kuychaco",
-  html_url VARCHAR DEFAULT NULL, -- "https://github.com/kuychaco",
-  organizations_url VARCHAR DEFAULT NULL, -- "https://api.github.com/users/kuychaco/orgs",
-  repos_url VARCHAR DEFAULT NULL, -- "https://api.github.com/users/kuychaco/repos",
-  name VARCHAR DEFAULT NULL, -- "Katrina Uychaco",
-  email VARCHAR DEFAULT NULL, -- "kuychaco@gmail.com",
-  created_at VARCHAR DEFAULT NULL, -- "2014-06-17T07:30:41Z",
-  updated_at VARCHAR DEFAULT NULL, -- "2015-05-15T18:04:53Z"
+  id integer, -- 7910250,  // PRIMARY KEY
+  login text, -- "kuychaco",
+  avatar_url text, -- "https://avatars.githubusercontent.com/u/7910250?v=3",
+  url text, -- "https://api.github.com/users/kuychaco",
+  html_url text, -- "https://github.com/kuychaco",
+  organizations_url text, -- "https://api.github.com/users/kuychaco/orgs",
+  repos_url text, -- "https://api.github.com/users/kuychaco/repos",
+  name text, -- "Katrina Uychaco",
+  email text, -- "kuychaco@gmail.com",
+  created_at text, -- "2014-06-17T07:30:41Z",
+  updated_at text, -- "2015-05-15T18:04:53Z",
+  repo_list integer ARRAY, -- [1,2]
   PRIMARY KEY (id)
 );
 
 
 -- ---
 -- Table 'repos'
---   Synced with GitHub API
+--   Foreign keys: none
 -- ---
 
 DROP TABLE IF EXISTS repos;
-    
+
 CREATE TABLE repos (
-/*TODO*/
+  id serial, -- 1,  // PRIMARY KEY
+  user_or_org text, -- "relentlessbreakfast",
+  name text, -- "sampleGraph"
+  PRIMARY KEY (id)
 );
+
 
 -- ---
 -- Table 'labels'
---   Synced with GitHub API
+--   Foreign keys: repo_id
 -- ---
 
 DROP TABLE IF EXISTS labels;
-    
+
 CREATE TABLE labels (
-  id SERIAL,
-  url VARCHAR DEFAULT NULL,
-  name VARCHAR DEFAULT NULL,
-  color VARCHAR DEFAULT NULL,
-  repo_id VARCHAR,
+  id serial, -- 1  // PRIMARY KEY
+  url text, -- "https://api.github.com/repos/relentlessbreakfast/sampleGraph/labels/bug",
+  name text, -- "bug",
+  color text, -- "fc2929",
+  repo_id integer, -- "1"
   PRIMARY KEY (id)
 );
 
--- ---
--- Table 'issues'
---   Synced with GitHub API
---   Foreign Keys:
---     (id_github_user) REFERENCES users (id_github);
---     (id_github_assignee) REFERENCES users (id_github);
---     (id_labels_array) REFERENCES labels (id);
--- ---
-
-DROP TABLE IF EXISTS issues;
-    
-CREATE TABLE issues (
-  id_github INTEGER,
-  html_url VARCHAR DEFAULT NULL,
-  api_url VARCHAR DEFAULT NULL,
-  numb INTEGER DEFAULT NULL,  -- changed 'number' to 'numb'
-  title VARCHAR DEFAULT NULL,
-  id_github_user INTEGER DEFAULT NULL,
-  id_github_assignee INTEGER DEFAULT NULL,
-  id_labels_array INTEGER ARRAY DEFAULT NULL,
-  state VARCHAR DEFAULT NULL,
-  locked VARCHAR DEFAULT NULL,
-  created_at VARCHAR DEFAULT NULL,
-  updated_at VARCHAR DEFAULT NULL,
-  closed_at VARCHAR DEFAULT NULL,
-  body VARCHAR DEFAULT NULL,
-  PRIMARY KEY (id_github)
-);
-
--- ---
--- Table 'clusters'
---   NOT synced with GitHub API
---   Foreign Keys:
---     (id_nodes_upstream_array) REFERENCES nodes (id);
---     (id_nodes_downstream_array) REFERENCES nodes (id);
---     (id_nodes_endpoints_array) REFERENCES nodes (id);
---     (id_github_creator) REFERENCES users (id_github);
--- ---
-
-DROP TABLE IF EXISTS clusters;
-    
-CREATE TABLE clusters (
-  id INTEGER,
-  abbrev VARCHAR DEFAULT 'NULL',
-  name VARCHAR DEFAULT NULL,
-  description VARCHAR DEFAULT NULL,
-  id_nodes_endpoints_array INTEGER ARRAY DEFAULT NULL,
-  id_github_creator INTEGER DEFAULT NULL,
-  PRIMARY KEY (id)
-);
-
--- ---
--- Table 'nodes'
---   NOT synced with GitHub API
---   Foreign Keys:
---     (id_parent) REFERENCES clusters (id);
---     (id_clusters) REFERENCES clusters (id);
---     (id_github_issues) REFERENCES issues (id_github);
---     (id_nodes_upstream_array) REFERENCES nodes (id);
---     (id_nodes_downstream_array) REFERENCES nodes (id);
--- ---
-
-DROP TABLE IF EXISTS nodes;
-    
-CREATE TABLE nodes (
-  id INTEGER,
-  type VARCHAR DEFAULT NULL,
-  id_parent INTEGER DEFAULT NULL,
-  id_clusters INTEGER DEFAULT NULL,
-  id_github_issues INTEGER DEFAULT NULL,
-  id_nodes_upstream_array INTEGER ARRAY DEFAULT NULL,
-  id_nodes_downstream_array INTEGER ARRAY DEFAULT NULL,
-  PRIMARY KEY (id)
-);
 
 -- ---
 -- Table 'comments'
---   Synced with GitHub API
---   Foreign Keys:
---     (id_github_users) REFERENCES users (id_github);
+--   Foreign keys: creator
 -- ---
 
 DROP TABLE IF EXISTS comments;
-    
+
 CREATE TABLE comments (
-  id_github INTEGER,
-  api_url VARCHAR DEFAULT NULL,
-  html_url VARCHAR DEFAULT NULL,
-  issue_url VARCHAR DEFAULT NULL,
-  created_at VARCHAR DEFAULT NULL,
-  updated_at VARCHAR DEFAULT NULL,
-  body VARCHAR DEFAULT NULL,
-  id_github_users INTEGER DEFAULT NULL,
-  PRIMARY KEY (id_github)
+  id integer, -- 106990292,  // PRIMARY KEY
+  url text, -- "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/comments/106990292",
+  html_url text, -- "https://github.com/relentlessbreakfast/sampleGraph/issues/7#issuecomment-106990292",
+  issue_url text, -- "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/7",
+  creator integer, -- 7910250,
+  created_at text, -- "2015-05-30T05:03:44Z",
+  updated_at text, -- "2015-05-30T05:03:44Z",
+  body text, -- "Comment 1"
+  PRIMARY KEY (id)
 );
 
--- ---
--- Foreign Keys 
--- ---
-
--- TODO: Syntax for array element foreign keys not working
---   http://blog.2ndquadrant.com/postgresql-9-3-development-array-element-foreign-keys/
-ALTER TABLE nodes ADD FOREIGN KEY (id_parent) REFERENCES clusters (id);
-ALTER TABLE nodes ADD FOREIGN KEY (id_clusters) REFERENCES clusters (id);
-ALTER TABLE nodes ADD FOREIGN KEY (id_github_issues) REFERENCES issues (id_github);
--- ALTER TABLE nodes ADD FOREIGN KEY (id_nodes_upstream_array) ELEMENT REFERENCES nodes;
--- ALTER TABLE nodes ADD FOREIGN KEY (id_nodes_downstream_array) ELEMENT REFERENCES nodes;
--- ALTER TABLE clusters ADD FOREIGN KEY (id_nodes_upstream_array) ELEMENT REFERENCES nodes;
--- ALTER TABLE clusters ADD FOREIGN KEY (id_nodes_downstream_array) ELEMENT REFERENCES nodes;
--- ALTER TABLE clusters ADD FOREIGN KEY (id_nodes_endpoints_array) ELEMENT REFERENCES nodes;
-ALTER TABLE clusters ADD FOREIGN KEY (id_github_creator) REFERENCES users (id_github);
--- ALTER TABLE issues ADD FOREIGN KEY (id_nodes_upstream_array) ELEMENT REFERENCES nodes;
--- ALTER TABLE issues ADD FOREIGN KEY (id_nodes_downstream_array) ELEMENT REFERENCES nodes;
-ALTER TABLE issues ADD FOREIGN KEY (id_github_user) REFERENCES users (id_github);
-ALTER TABLE issues ADD FOREIGN KEY (id_github_assignee) REFERENCES users (id_github);
--- ALTER TABLE issues ADD FOREIGN KEY (id_labels_array) ELEMENT REFERENCES labels;
-ALTER TABLE comments ADD FOREIGN KEY (id_github_users) REFERENCES users (id_github);
-
-
 
 -- ---
--- Test Data
+-- Table 'issues'
+--   Foreign keys: creator, assignee, labels elements
 -- ---
 
--- INSERT INTO nodes (id,type,parent,id_clusters,id_github_issues,id_nodes_upstream_array,id_node ARRAYs_downstream_array) VALUES ARRAY
--- ('','','','','','','');
--- INSERT INTO clusters (id,code,text,description,id_nodes_upstream_array,id_node ARRAYs_downstream_array,id_node ARRAYs_endpoints_array,id_gith ARRAYub_creator) VALUES
--- ('','','','','','','','');
--- INSERT INTO issues (id_github,id_nodes_upstream_array,id_node ARRAYs_downstream_array,html_ur ARRAYl,api_url,number,title,id_github_user,id_github_assignee,id_labels_array,state,l ARRAYocked,created_at,updated_at,closed_at,body) VALUES
--- ('','','','','','','','','','','','','','','','');
--- INSERT INTO users (id_github,login,avatar_url,api_url,html_url) VALUES
--- ('','','','','');
--- INSERT INTO labels (id,url,name,color) VALUES
--- ('','','','');
--- INSERT INTO comments (id_github,api_url,html_url,issue_url,created_at,updated_at,body,id_github_users) VALUES
--- ('','','','','','','','');
+-- TODO: HANDLE COMMENTS
+
+DROP TABLE IF EXISTS issues;
+
+CREATE TABLE issues (
+  id integer, -- 82639733, // PRIMARY KEY
+  url text, -- "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/7",
+  labels_url text, -- "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/7/labels{/name}",
+  comments_url text, -- "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/7/comments",
+  events_url text, -- "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/7/events",
+  html_url text, -- "https://github.com/relentlessbreakfast/sampleGraph/issues/7",
+  number_github integer, -- 7,
+  title text, -- "Make sample graph data",
+  creator integer, -- 1445825,
+  labels integer ARRAY, -- [6],
+  state text, -- "open",
+  locked boolean, -- false,
+  assignee integer, -- 1445825,
+  comments integer, -- 2,
+  created_at text, -- "2015-05-30T00:18:26Z",
+  updated_at text, -- "2015-05-30T00:43:54Z",
+  closed_at text, -- null,
+  body text, -- "type:\ * Issue\ \ Upstream:\ * entry\ \ Downstream:\ * Cluster-Repo Selection Screen\ * Make sample graph data"
+  PRIMARY KEY (id)
+);
+
+
+
+-- ---
+-- Table 'clusters'
+--   Foreign keys: endponts elements, creator
+-- ---
+
+DROP TABLE IF EXISTS clusters;
+
+CREATE TABLE clusters (
+  id serial, -- 1,  // PRIMARY KEY
+  abbrev text, -- "ROOT",  // must be less than 32 chars
+  name text, -- "Project Root",
+  description text, -- "Cluster of entire project",
+  endpoints integer ARRAY, -- [2, 3],  // these foreign key IDs for entries in NODES table
+  creator integer, -- 1445825,  // foreign key ID for entry in USERS table
+  -- deleted integer ARRAY, -- []
+  PRIMARY KEY (id)
+);
+
+
+
+-- ---
+-- Table 'nodes'
+--   Foreign keys: cluster_id, issue_id, upsream_nodes, downstream_nodes
+-- ---
+
+-- TODO: refactor all_upstream to hstore data type
+DROP TABLE IF EXISTS nodes;
+
+CREATE TABLE nodes (
+  id serial, -- 1, // PRIMARY KEY
+  type text, -- "cluster",
+  parent_cluster integer, -- NULL, // foreign key ID from NODES table
+  cluster_id integer, -- 1, // foreign key ID from CLUSTERS table
+  issue_id integer, -- NULL, // foreign key ID from ISSUES table
+  upstream_nodes integer, -- [], // foreign key ID from NODES table
+  downstream_nodes integer, -- [], // foreign key ID from NODES table
+  -- all_upstream json, -- {}
+  -- all_upstream hstore, -- {}
+  PRIMARY KEY (id)
+);
+
+
+ALTER TABLE labels ADD FOREIGN KEY (repo_id) REFERENCES repos (id);
+ALTER TABLE comments ADD FOREIGN KEY (creator) REFERENCES users (id);
+ALTER TABLE issues ADD FOREIGN KEY (creator) REFERENCES users (id);
+ALTER TABLE issues ADD FOREIGN KEY (assignee) REFERENCES users (id);
+ALTER TABLE clusters ADD FOREIGN KEY (creator) REFERENCES users (id);
+ALTER TABLE nodes ADD FOREIGN KEY (cluster_id) REFERENCES clusters (id);
+ALTER TABLE nodes ADD FOREIGN KEY (issue_id) REFERENCES issues (id);
+
 
