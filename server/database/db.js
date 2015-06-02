d7b9e34f83ec7f8172921199ff24fac2844034bf
Katrina Uychaco <kuychaco@gmail.com>
2015-06-01 21:57:26 -0700

    (refactor) Implement promises

--- server/database/db.js
+++ server/database/db.js
@@ -2,35 +2,52 @@
 * @Author: kuychaco
 * @Date:   2015-05-28 15:33:28
 * @Last Modified by:   kuychaco
-* @Last Modified time: 2015-06-01 11:55:32
+* @Last Modified time: 2015-06-01 17:02:09
 */
 
 'use strict';
 
-var pg = require('pg');
-var fs = require('fs');
+var config = require('../../build-config');
 
-// var conString = "postgres://root:root@localhost/constellation";
+var pg = require('pg');
+
+var Promise = require('bluebird');
+var readFile = Promise.promisify(require('fs').readFile);
+
 // create constellation database
+// var conString = "postgres://root:root@localhost/constellation";
 var conString = "postgres://localhost:5432/constellation";
 
 // TODO: explore client pooling ###########
-// connect a single client to a postgres instance, run some queries, and disconnect
-var client = new pg.Client(conString);
+// connect a single client to a postgres instance and use to interact with the database
+var client = Promise.promisifyAll(new pg.Client(conString));
 
-fs.readFile('/Users/kuychaco/Engineering/Constellation/constellation/server/database/postgres-schema.sql', {encoding: 'utf8'}, function(err, schema) {
-  if (err) {
-    return console.error('could not read schema file', err);
-  }
-  client.connect(function(err) {
-    if (err) {
-      return console.error('could not connect to postgres', err);
-    }
-    client.query(schema, function(err, result) {
-      if (err) {
-        return console.error('error loading schema', err);
-      }
-      client.end();
-    });
+var schema;
+readFile(config.db+'/postgres-schema.sql', 'utf8')
+  .then(function(schemaFile) {
+    schema = schemaFile;
+    // establish connection to database
+    return client.connectAsync();
+  })
+  .catch(function(err) {
+    console.error('could not read schema file', err);
+  })
+  .then(function() {
+    // load database schema
+    return client.queryAsync(schema);
+  })
+  .catch(function(err) {
+    console.error('could not connect to postgres', err);
+  })
+  .then(function(result) {
+    console.log('Databse schema loaded');
+  })
+  .catch(function(err) {
+    console.error('error loading schema', err);
+  })
+  .finally(function() {
+    // close connection
+    client.endAsync();
   });
-});
+
+module.exports = client;
