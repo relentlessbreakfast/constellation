02db47d84afab4fc27e93c539e94a0b5f0679ce7
Katrina Uychaco <kuychaco@gmail.com>
2015-06-02 19:26:45 -0700

    Merge conflict

--- server/database/db.js
+++ server/database/db.js
@@ -9,9 +9,7 @@
 var config = require('../../build-config');
 
 var pg = require('pg');
-var fs = require('fs');
 
-<<<<<<< HEAD
 var Promise = require('bluebird');
 var readFile = Promise.promisify(require('fs').readFile);
 
@@ -52,29 +50,4 @@ readFile(config.db+'/postgres-schema.sql', 'utf8')
   });
 
 module.exports = client;
-=======
-// var conString = "postgres://root:root@localhost/constellation";
-// create constellation database
-var conString = "postgres://localhost:5432/constellation";
 
-// TODO: explore client pooling ###########
-// connect a single client to a postgres instance, run some queries, and disconnect
-var client = new pg.Client(conString);
-
-fs.readFile('/Users/kuychaco/Engineering/Constellation/constellation/server/database/postgres-schema.sql', {encoding: 'utf8'}, function(err, schema) {
-  if (err) {
-    return console.error('could not read schema file', err);
-  }
-  client.connect(function(err) {
-    if (err) {
-      return console.error('could not connect to postgres', err);
-    }
-    client.query(schema, function(err, result) {
-      if (err) {
-        return console.error('error loading schema', err);
-      }
-      client.end();
-    });
-  });
-});
->>>>>>> (feat) Load schema into database
