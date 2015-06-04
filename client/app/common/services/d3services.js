d26c4c612a27b5206b1604d35d16acfb949a9e49
cwhwang1986 <cwhwang1986@gmail.com>
2015-06-04 15:00:22 -0700

    (refactor) Move the service file to services folder

---
+++ client/app/common/services/d3services.js
@@ -0,0 +1,19 @@
+'use strict';
+(function (angular) {
+
+  var D3Service = function ($window) {
+    return {
+      getD3 : function () {
+        return $window.d3;
+      },
+      getDagreD3 : function() {
+        return $window.dagreD3;
+      }
+    };
+  };
+
+  angular
+    .module('cd-app.common')
+    .factory('D3Service', D3Service);
+
+})(angular);
