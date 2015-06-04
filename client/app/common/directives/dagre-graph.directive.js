83c3c8aebe0a0048d70ef06effed2ba90faf7d31
cwhwang1986 <cwhwang1986@gmail.com>
2015-06-03 17:07:24 -0700

    (refactor) Change the folder/file name from graph to graphPanel

---
+++ client/app/common/directives/dagre-graph.directive.js
@@ -0,0 +1,40 @@
+/* 
+* @Author: justinwebb
+* @Date:   2015-06-03 15:30:09
+* @Last Modified by:   ChalrieHwang
+* @Last Modified time: 2015-06-03 16:59:04
+*/
+
+'use strict';
+(function (angular) {
+
+  var GraphPanelCtrl = function ($scope) {
+
+  };
+
+  var link = function ($scope, elem, attr) {
+    console.log('GraphPanelDirective: ', elem);
+  };
+
+  var GraphPanelDirective = function () {
+    
+    return {
+      restrict: 'E',
+      scope: {
+      },
+      link: link,
+      controller: GraphPanelCtrl,
+      template:  [
+        '<div class="graph">',
+        ' <h2>This is the graph page!</h2>',
+        ' <svg id="canvas"><g/></svg>',
+        '</div>'
+      ].join(',')
+    };
+  };
+
+
+  angular
+    .module('cd-app.common')
+    .directive('dagreGraphPanel', GraphPanelDirective);
+})(angular);
