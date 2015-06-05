efe0c7520e695ca2d180172252e818fbbebeba7c
cwhwang1986 <cwhwang1986@gmail.com>
2015-06-05 15:15:21 -0700

    (fix) Fix graph object property, add cluster id to function:createClusterNode

--- client/app/graphpanel/graph.js
+++ client/app/graphpanel/graph.js
@@ -2,7 +2,7 @@
 * @Author: ChalrieHwang
 * @Date:   2015-06-01 17:45:29
 * @Last Modified by:   ChalrieHwang
-* @Last Modified time: 2015-06-05 09:27:44
+* @Last Modified time: 2015-06-05 14:56:29
 */
 
 'use strict';
@@ -110,6 +110,7 @@
         class: nodeType,
         shape: 'circle'
       });
+      $scope.g.node(id).clusterId = jsonObj.cluster_id.id;
       $scope.g.node(id).description = jsonObj.cluster_id.description;
       $scope.g.node(id).creator = jsonObj.cluster_id.creator;
       $scope.g.node(id).parentCluster = jsonObj.parent_cluster;
@@ -133,7 +134,8 @@
 
     //Testing Data
     $scope.data = {
-      parent_cluster: {
+      parent_cluster : 1,
+      1: {
         "id": 1, // PRIMARY KEY
         "type": "cluster",
         "parent_cluster": null, // foreign key ID from NODES table
@@ -148,112 +150,6 @@
         "issue_id": null, // foreign key ID from ISSUES table
         "upstream_nodes": [], // foreign key ID from NODES table
         "downstream_nodes": [] // foreign key ID from NODES table
-      },
-      2: {
-        "id": 2,// PRIMARY KEY
-        "type": "entry",
-        "parent_cluster": 1, // foreign key ID from NODES table
-        "cluster_id": null, // foreign key ID from CLUSTERS table
-        "issue_id": null, // foreign key ID from ISSUES table
-        "upstream_nodes": null, // foreign key ID from NODES table
-        "downstream_nodes": [4,6] // foreign key ID from NODES table
-      },
-      3: {
-        "id": 3,// PRIMARY KEY
-        "type": "exit",
-        "parent_cluster": 1, // foreign key ID from NODES table
-        "cluster_id": null, // foreign key ID from CLUSTERS table
-        "issue_id": null, // foreign key ID from ISSUES table
-        "upstream_nodes": [5,7], // foreign key ID from NODES table
-        "downstream_nodes": [] // foreign key ID from NODES table
-      },
-      4: {
-        "id": 4,// PRIMARY KEY
-        "type": "issue",
-        "parent_cluster": 1, // foreign key ID from NODES table
-        "cluster_id": null, // foreign key ID from CLUSTERS table
-        "issue_id": {
-          "id": 82639324,
-          "url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/4",
-          "labels_url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/4/labels{/name}",
-          "comments_url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/4/comments",
-          "events_url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/4/events",
-          "html_url": "https://github.com/relentlessbreakfast/sampleGraph/issues/4",
-          "number": 4,
-          "title": "Add O-auth",
-          "user": 1445825,
-          "labels": [1],
-          "state": "open",
-          "locked": false,
-          "assignee": 442978,
-          "comments": 0,
-          "created_at": "2015-05-30T00:16:35Z",
-          "updated_at": "2015-05-30T00:44:37Z",
-          "closed_at": null,
-          "body": "Type:\ * issue\ \ Upstream:\ * entry\ \ Downstream:\ * Cluster-Repo Selection Screen\ * Make sample graph data"
-        }, // foreign key ID from ISSUES table
-        "upstream_nodes": [2], // foreign key ID from NODES table
-        "downstream_nodes": [5,7] // foreign key ID from NODES table
-      },
-      5: {
-        "id": 5,// PRIMARY KEY
-        "type": "cluster",
-        "parent_cluster": 1, // foreign key ID from NODES table
-        "cluster_id": {
-          "id": 3,  // PRIMARY KEY
-          "abbrev": "Repo selection",  // must be less than 32 chars
-          "name": "Cluster-Repo Selection Screen",
-          "description": "Cluster of repo selection related tasks",
-          "endpoints": [13, 14],  // these foreign key IDs for entries in NODES table
-          "creator": 1445825  // foreign key ID for entry in USERS table
-        }, // foreign key ID from CLUSTERS table
-        "issue_id": null, // foreign key ID from ISSUES table
-        "upstream_nodes": [4,6], // foreign key ID from NODES table
-        "downstream_nodes": [3] // foreign key ID from NODES table
-      },
-      6: {
-        "id": 6,// PRIMARY KEY
-        "type": "cluster",
-        "parent_cluster": 1, // foreign key ID from NODES table
-        "cluster_id": {
-          "id": 2,  // PRIMARY KEY
-          "abbrev": "DB",  // must be less than 32 chars
-          "name": "Cluster-Database Schema",
-          "description": "Cluster of database schema related tasks",
-          "endpoints": [11, 12],  // these foreign key IDs for entries in NODES table
-          "creator": 1445825  // foreign key ID for entry in USERS table
-        }, // foreign key ID from CLUSTERS table
-        "issue_id": null, // foreign key ID from ISSUES table
-        "upstream_nodes": [2], // foreign key ID from NODES table
-        "downstream_nodes": [5,7] // foreign key ID from NODES table
-      },
-      7: {
-        "id": 7,// PRIMARY KEY
-        "type": "issue",
-        "parent_cluster": 1, // foreign key ID from NODES table
-        "cluster_id": null, // foreign key ID from CLUSTERS table
-        "issue_id": {
-          "id": 82639733, // PRIMARY KEY
-          "url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/7",
-          "labels_url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/7/labels{/name}",
-          "comments_url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/7/comments",
-          "events_url": "https://api.github.com/repos/relentlessbreakfast/sampleGraph/issues/7/events",
-          "html_url": "https://github.com/relentlessbreakfast/sampleGraph/issues/7",
-          "number": 7,
-          "title": "Make sample graph data",
-          "user": 1445825,
-          "labels": [6],
-          "state": "open",
-          "locked": false,
-          "assignee": 1445825,
-          "comments": 2,
-          "created_at": "2015-05-30T00:18:26Z",
-          "updated_at": "2015-05-30T00:43:54Z",
-          "closed_at": null,
-          "body": "type:\ * Issue\ \ Upstream:\ * entry\ \ Downstream:\ * Cluster-Repo Selection Screen\ * Make sample graph data"
-        }, // foreign key ID from ISSUES table
-        "upstream_nodes": [4,6], // foreign key ID from NODES table
-        "downstream_nodes": [3] // foreign key ID from NODES table
       }
     };
     
@@ -263,30 +159,46 @@
     $scope.buildGraph = function(data){
       if($scope.g !== undefined){
         $scope.g = $scope.createCanvas();
+        var parentId = data.parent_cluster;
+        //Create Nodes
         _.each(data, function(obj, key){
           var tp = obj.type;
           if(tp === 'issue' || tp === 'entry' || tp === 'exit'){
             $scope.createIssueNode(obj);
-          } else if (tp === 'cluster' && key !== 'parent_cluster'){
+          } else if (tp === 'cluster' && key != parentId){
             $scope.createClusterNode(obj);
           }
         });
+        //Create Edges
         _.each(data, function(obj, key){
           var tp = obj.type;
           if(tp === 'issue' || tp ==='entry' || tp === 'exit'){
             $scope.createEdge(obj.id);
-          } else if (tp === 'cluster' && key !== 'parent_cluster'){
+          } else if (tp === 'cluster' && key != parentId){
             $scope.createEdge(obj.id);
           } 
         });
+
+        if(Object.keys(data).length > 2){
+          $scope.renderGraph($scope.g);
+          $scope.createClusterNode(data[parentId]);
+        } else {
+          $scope.createClusterNode(data[parentId]);
+          $scope.renderGraph($scope.g);
+        }
       } else {
         $scope.g = $scope.createCanvas();
-        $scope.createClusterNode(data.parent_cluster);
+        $scope.createClusterNode(data[1]);
+        $scope.renderGraph($scope.g);
       }
-      $scope.renderGraph($scope.g);
     };
 
-    $scope.buildGraph($scope.data);
+    /**
+     * Watch the chagne of the data object and rerender the graph
+     */
+    $scope.$watchCollection('data', function(newVal, oldVal){
+      $scope.buildGraph(newVal);
+    });
   };
 
 
