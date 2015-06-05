1b876f9f4acba1c2aa695d64955c98adec0bff3d
cwhwang1986 <cwhwang1986@gmail.com>
2015-06-05 15:13:50 -0700

    (feat) Implement mouse click function for clicking entry/exit nodes

--- client/app/common/directives/dagre-graph.directive.js
+++ client/app/common/directives/dagre-graph.directive.js
@@ -2,7 +2,7 @@
 * @Author: justinwebb
 * @Date:   2015-06-03 15:30:09
 * @Last Modified by:   ChalrieHwang
-* @Last Modified time: 2015-06-05 13:00:10
+* @Last Modified time: 2015-06-05 15:11:21
 */
 
 'use strict';
@@ -11,53 +11,13 @@
   var GraphPanelCtrl = function ($scope, GraphService) {
     var graphScope = $scope.$parent;
 
-    $scope.onGraphDblClick = function($event){
-      var clickObjType = $event.path[0].tagName;
-      var nodeId,
-          nodeClass,
-          abbrev;
-
-      if(clickObjType === 'path'){
-        var upNodeId = $event.target.__data__.v;
-        var downNodeId = $event.target.__data__.w;
-      } else if (clickObjType === 'circle'){
-        nodeId = $event.target.__data__;
-        nodeClass = graphScope.g.node(nodeId).class;
-        abbrev = graphScope.g.node(nodeId).label;
-      } else if (clickObjType === 'tspan'){
-        nodeId = $event.path[4].__data__;
-        nodeClass = graphScope.g.node(nodeId).class;
-        abbrev = graphScope.g.node(nodeId).label;
-      } else {
-        
-      }
-      //click cluster node
-      if(nodeClass === 'cluster' || abbrev !== 'ROOT'){
-        var clusterId = graphScope.g.node(nodeId).clusterId.id;
-        var promise = GraphService.getGraph(clusterId);
-        promise.then(function(){
-            $scope.$parent.data = GraphService.graphObj;
-          }, function(err){
-            console.log('error');
-          });
-      //click entry exit nodes
-      } else if (nodeClass === 'entry' || nodeClass === 'exit') {
-        var parentClusterId = graphScope.g.node(nodeId).parentCluster;
-        var promise = GraphService.getGraph(parentClusterId);
-        console.log('promise', promise);
-        promise.then(function(result){
-            $scope.$parent.data = GraphService.graphObj;
-          }, function(err){
-            console.log('error');
-          });
-      } 
-    };
-
     $scope.onGraphDblClick = function($event, $q){
       var clickObjType = $event.path[0].tagName;
+      var nodeClasses = ['cluster', 'entry', 'exit'];
       var nodeId,
           nodeClass,
-          abbrev;
+          abbrev,
+          promise;
       if(clickObjType === 'path'){
         var upNodeId = $event.target.__data__.v;
         var downNodeId = $event.target.__data__.w;
@@ -73,19 +33,20 @@
         
       }
       //click cluster
-      if(nodeClass === 'cluster'){
-        var clusterId = graphScope.g.node(nodeId).clusterId;
-        var promise = GraphService.getGraph(clusterId);
+      if(nodeClasses.indexOf(nodeClass) !== -1){
+        if(nodeClass === 'cluster'){
+          var clusterId = graphScope.g.node(nodeId).clusterId;
+          promise = GraphService.getGraph(clusterId);
+        } else {
+          var parentId = graphScope.g.node(nodeId).parentCluster;
+          var parentClusterId = graphScope.g.node(parentId).parentCluster;
+          if(parentClusterId === null){
+            promise = GraphService.getGraph('root');
+          } else {
+            promise = GraphService.getGraph(parentClusterId);
+          } 
+        }
         promise.then(function(obj){
-            $scope.$parent.data = GraphService.graphObj;
-          }, function(err){
-            console.log('error');
-          });
-      //click entry exit
-      } else if (nodeClass === 'entry' || nodeClass === 'exit') {
-        var parentClusterId = graphScope.g.node(nodeId).parentCluster;
-        var promise = GraphService.getGraph(parentClusterId);
-        promise.then(function(){
             $scope.$parent.data = GraphService.graphObj;
           }, function(err){
             console.log('error');
