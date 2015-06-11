bdcd7b2d15fc4ada5bb34b0f4a606b2d3f0ef488
Katrina Uychaco <kuychaco@gmail.com>
2015-06-11 00:14:46 -0700

    (fix) Add array type to up/downstream nodes

--- server/database/postgres-schema.sql
+++ server/database/postgres-schema.sql
@@ -131,8 +131,8 @@ CREATE TABLE IF NOT EXISTS nodes (
   parent_cluster integer, -- NULL, // foreign key ID from NODES table
   cluster_id integer REFERENCES clusters (id), -- 1, // foreign key ID from CLUSTERS table
   issue_id integer REFERENCES issues (id), -- NULL, // foreign key ID from ISSUES table
-  upstream_nodes integer, -- [], // foreign key ID from NODES table
-  downstream_nodes integer, -- [], // foreign key ID from NODES table
+  upstream_nodes integer ARRAY, -- [], // foreign key ID from NODES table
+  downstream_nodes integer ARRAY, -- [], // foreign key ID from NODES table
   PRIMARY KEY (id)
 );
 
