9a083df31162800c75386ff8e49a7b96bd13321f
cwhwang1986 <cwhwang1986@gmail.com>
2015-06-11 20:22:11 -0700

    (feat) Add complete status to the information panel

--- client/app/infopanel/info-panel.js
+++ client/app/infopanel/info-panel.js
@@ -2,18 +2,39 @@
 * @Author: Justin Webb
 * @Date:   2015-06-11 13:00:38
 * @Last Modified by:   ChalrieHwang
-* @Last Modified time: 2015-06-11 18:40:44
+* @Last Modified time: 2015-06-11 20:04:04
 */
 
 'use strict';
 (function (angular) {
 
 var InfoPanelController = function ($scope) {
+  var $scope.nodeId = $scope.rightClickId;
+  $scope.$watchCollection('nodeId', function(newVal){
+    if(newVal){
+      $scope.buildGraph(newVal);
+    }
+  });
+  var renderInfo = function(id){
+    var objClass = $scope.g.node(id).class;
+    if(nodeClass === 'issue'){
+      $scope.displayId = '#'+ $scope.g.node(nodeId).label;
+      $scope.displayTitle = $scope.g.node(nodeId).title;
+      $scope.displayDescription = $scope.g.node(nodeId).description;
+      $scope.displayState = $scope.g.node(nodeId).status;
+      $scope.displayAssignee = $scope.g.node(nodeId).asignee;
+      $scope.displayIssuetype = $scope.g.node(id).issueType;
+    } else if (nodeClass === 'cluster'){
+      $scope.display = true;
+      $scope.displayId = $scope.g.node(nodeId).abbrev;
+      $scope.displayTitle = $scope.g.node(nodeId).title;
+      $scope.displayDescription = $scope.g.node(nodeId).description;
+      $scope.displayCount = $scope.g.node(nodeId).childrenCount;
+      $scope.displayComplete = Number($scope.g.node(nodeId).complete / 
+        $scope.displayCount * 100).toFixed(1);
+    }
 
-
-
-  
-  console.log('InfoPanelController scope:', $scope.graphData);
+  }
 };
 
 
